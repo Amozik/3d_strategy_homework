b62974584f1bcc6e8c6c059a6aa98209e6c094a0
Sergey Amozov <sergey_amozov@belkatechnologies.com>
2021-07-25 15:23:46 +0200

    add selection and right ui

---
+++ Assets/Scripts/UserControlSystem/UI/Presenter/MouseInteractionsPresenter.cs
@@ -0,0 +1,35 @@
+using System.Linq;
+using Abstractions;
+using UnityEngine;
+using UserControlSystem.UI.Model;
+
+namespace UserControlSystem.UI.Presenter
+{
+    public sealed class MouseInteractionsPresenter : MonoBehaviour
+    {
+        [SerializeField] 
+        private Camera _camera;
+        [SerializeField] 
+        private SelectableValue _selectedObject;
+    
+        private void Update()
+        {
+            if (!Input.GetMouseButtonUp(0))
+            {
+                return;
+            }
+        
+            var hits = Physics.RaycastAll(_camera.ScreenPointToRay(Input.mousePosition));
+            if (hits.Length == 0)
+            {
+                return;
+            }
+        
+            var selectable = hits
+                .Select(hit => hit.collider.GetComponentInParent<ISelectable>())
+                .FirstOrDefault(component => component != null);
+        
+            _selectedObject.SetValue(selectable);
+        }
+    }
+}

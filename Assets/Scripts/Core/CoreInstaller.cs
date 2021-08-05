397556d76c4ef29ee07d61ffca2289d2a1bd1c19
Sergey Amozov <sergey_amozov@belkatechnologies.com>
2021-08-05 22:24:54 +0200

    add UniRx

---
+++ Assets/Scripts/Core/CoreInstaller.cs
@@ -0,0 +1,13 @@
+﻿using Zenject;
+using Core.Timers;
+
+namespace Core
+{
+    public class CoreInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            Container.BindInterfacesAndSelfTo<TimeModel>().AsSingle();
+        }
+    }
+}
